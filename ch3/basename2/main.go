f1212efe8bf6d3ef1771e4fcc04368b63a85c248
Richard Li <richardli125@gmail.com>
2021-01-18 15:39:14 -0500

    added basename2 example

---
+++ ch3/basename2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		fmt.Println(basename(input.Text()))
+	}
+}
+
+// a/b/c.go
+
+func basename(s string) string {
+	slash := strings.LastIndex(s, "/") // -1 if "/" not found
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
