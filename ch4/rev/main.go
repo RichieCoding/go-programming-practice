a928158a6c915dbaaa92bfaafc174bf8d31f40e5
Richard Li <richardli125@gmail.com>
2021-01-24 23:51:51 -0500

    added chapter 4

---
+++ ch4/rev/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	//!+array
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	reverse(a[:])
+	// fmt.Println(a)
+
+	//!+slice
+	s := []int{0, 1, 2, 3, 4, 5}
+	// Rotate s left by two positions.
+	fmt.Println(s)
+	reverse(s[:2]) // [0, 1]
+	fmt.Println(s)
+	reverse(s[2:]) // [2, 3, 4, 5]
+	fmt.Println(s)
+	reverse(s) // 
+	fmt.Println(s) // [2 3 4 5 0 1]
+}
+
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
