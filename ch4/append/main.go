3a2bdbaac24f57f6b6d3119b981c3f75297632ab
Richard Li <richardli125@gmail.com>
2021-01-31 02:30:19 -0500

    added appendSlice function

--- ch4/append/main.go
+++ ch4/append/main.go
@@ -1,7 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var x, y []int
+	for i := 0; i < 10; i++ {
+		y = appendInt(x, i)
+		fmt.Printf("%d cap=%d\t%v\n", i, cap(y), y)
+		x = y
+	}
 }
 
 func appendInt(x []int, y int) []int {
@@ -21,5 +28,25 @@ func appendInt(x []int, y int) []int {
 		copy(z, x)
 	}
 	z[len(x)] = y
+	return z
+}
+
+func appendSlice(x []int, y ...int) []int {
+	var z []int
+	zlen := len(x) + len(y)
+	if zlen < cap(x) {
+		// There is room to expand the slice
+		z = x[:zlen]
+	} else {
+		// There is insufficient space
+		// Grow by doubling, for amoritzed linear complexity 
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	copy(z[len(x):], y)
 	return z
 }
