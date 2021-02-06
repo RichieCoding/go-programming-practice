3b614475e8918d4b11b74e3e8dbbff769eb719d6
Richard Li <richardli125@gmail.com>
2021-02-06 15:49:08 -0500

    added nonempty example

---
+++ ch4/nonempty/main.go
@@ -0,0 +1,27 @@
+package main
+
+func main() {
+	data := []string{"one", "", "three"}
+	fmt.Printf("%q\n", nonempty(data)) // `["one", "three"]`
+}
+
+func nonempty(strings []string) []string {
+	i := 0
+	for _, s := range strings {
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+	return strings[:i]
+}
+
+func nonempty2(strings []string) []string {
+	out := strings[:0] // zero-length slice of original
+	for _, s := range strings {
+		if s !== "" {
+			out = append(out, s)
+		}
+	}
+	return out
+}
