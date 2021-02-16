a7f3dbc39dd0bf6833c4ce5550e9a4fa810c5ddc
Richard Li <richardli125@gmail.com>
2021-02-15 21:58:07 -0500

    added treesort example

---
+++ ch4/treesort/sort.go
@@ -0,0 +1,40 @@
+// Package treesort provides insertion sort using an unblanced binary tree
+package treesort
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+// Sort sorts values in place
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	// appendValues()
+}
+
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		// Equivalent to return &tree{value: value}
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
